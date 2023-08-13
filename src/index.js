9b276ffd0c83bc5223922fddc0a71fa02dd024f5
Caleb Stark <calebfustos7@gmail.com>
2023-08-13 01:29:45 -0500

    Adding in files to the Reop that were already created

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const board = document.querySelector(".puzzleBoard");
+
+for (let i = 0; i < 9; i++){
+    const largeSqure = document.createElement("section");
+    const largeClass = document.cl
+}
