fa8ce3625ffc96a3056fb262851f3788d2c48ace
Caleb Stark <calebfustos7@gmail.com>
2023-08-14 01:58:42 -0500

    Styling is basic and kinda ugly but set in a way I want it to be layed out once complete. Was also able to confirm dom connection and able to verify input from user.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-const board = document.querySelector(".puzzleBoard");
-
-for (let i = 0; i < 9; i++){
-    const largeSqure = document.createElement("section");
-    const largeClass = document.cl
+function verifyInput(event) {
+    const eventData = event.data;
+    if (!isNaN(eventData)){
+        console.log("It's a number");
+        if (eventData === "0"){
+            console.log("It's a 0 try again");
+        } else {
+            console.log("It's a valid Number");
+        }
+    } else {
+        console.log("It's not a number");
+    }
 }
+
+function main() {
+    const userInputs = document.querySelectorAll(".squareInput")
+    for(let userInput of userInputs){
+        userInput.addEventListener("input", verifyInput)
+    }
+};
+
+window.addEventListener("DOMContentLoaded", main);
