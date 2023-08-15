3c6852cc4be22412666d2109c626ddc6f71c597f
Caleb Stark <calebfustos7@gmail.com>
2023-08-15 01:24:23 -0500

    Added alert boxes for invalid input into input boxes and began setting up initial array to track input for the puzzleboard

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
+let puzzleBoard = {largeBox1:[{row1:[],row2:[],row3:[]},{col1:[],col2:[],col3:[]}] 
+    , largeBox2:[{row1:[],row2:[],row3:[]},{col4:[],col5:[],col6:[]}] 
+    , largeBox3:[{row1:[],row2:[],row3:[]},{col7:[],col8:[],col9:[]}] 
+    , largeBox4:[{row4:[],row5:[],row6:[]},{col1:[],col2:[],col3:[]}] 
+    , largeBox5:[{row4:[],row5:[],row6:[]},{col4:[],col5:[],col6:[]}] 
+    , largeBox6:[{row4:[],row5:[],row6:[]},{col7:[],col8:[],col9:[]}]
+    , largeBox7:[{row7:[],row8:[],row9:[]},{col1:[],col2:[],col3:[]}] 
+    , largeBox8:[{row7:[],row8:[],row9:[]},{col4:[],col5:[],col6:[]}] 
+    , largeBox9:[{row7:[],row8:[],row9:[]},{col7:[],col8:[],col9:[]}]}
+
+function addInputToArray(eventData, eventId){
+    console.log(eventData)
+    console.log(eventId)
+    
+}
+
 function verifyInput(event) {
+    const eventId = document.getElementById(event.target.id)
     const eventData = event.data;
     if (!isNaN(eventData)){
-        console.log("It's a number");
         if (eventData === "0"){
-            console.log("It's a 0 try again");
+            alert("0 is not valid: Please enter any number 1-9");
         } else {
-            console.log("It's a valid Number");
+            return addInputToArray(eventData, eventId);
         }
     } else {
-        console.log("It's not a number");
-    }
+        alert("Input is not valid: Please enter any number 1-9");
+    };
 }
 
 function main() {
